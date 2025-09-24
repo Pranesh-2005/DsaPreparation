e31d6775a7e2d4a2ff48719b55a18c0bc65e2670
selvaganesh19 <selvaganesh.al22@bitsathy.ac.in>
2025-09-24 14:44:25 +0530

    Added solutions for MaxDepthBinaryTree
    Co-authored-by: PRANESH S <praneshmadhan646@gmail.com>

---
+++ src/MaxDepthBinaryTree.java
@@ -0,0 +1,7 @@
+public class MaxDepthBinaryTree {
+    static class TreeNode { int val; TreeNode left, right; TreeNode(int v){val=v;} }
+    public int maxDepth(TreeNode root) {
+        if (root==null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
