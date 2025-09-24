5bf4083918d059091f1213d0d21c3357ce30d54e
selvaganesh19 <selvaganesh.al22@bitsathy.ac.in>
2025-09-24 14:52:15 +0530

    Added solutions for LowestCommonAncestorBST
    Co-authored-by: PRANESH S <praneshmadhan646@gmail.com>

---
+++ src/LowestCommonAncestorBST.java
@@ -0,0 +1,11 @@
+public class LowestCommonAncestorBST {
+    static class TreeNode { int val; TreeNode left,right; TreeNode(int v){val=v;} }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) root = root.left;
+            else if (p.val > root.val && q.val > root.val) root = root.right;
+            else return root;
+        }
+        return null;
+    }
+}
